0eeb64f1cd7a2e17de1b2605f0b76d9ac2358569
Chance Rutledge <chancerutledge2@gmail.com>
2019-12-20 08:40:43 -0800

    Authentication, Login, protectedroute implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
+
+
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute path="/protected" component={BubblePage} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Route exact path="/" component={Login} />
+//         {/* 
+//           Build a PrivateRoute component that will 
+//           display BubblePage when you're authenticated 
+//         */}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
